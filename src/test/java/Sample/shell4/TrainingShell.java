c16d530719e3d16980095ef03986cdb5b49dd89f
Jeswant Fernandez <srinvs.gmail.com>
2022-08-24 21:59:02 +0530

    Firt commit

---
+++ src/test/java/Sample/shell4/TrainingShell.java
@@ -0,0 +1,41 @@
+package Sample.shell4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TrainingShell {
+
+	
+	@Test //Annotation
+	public void login()
+	{
+		System.out.println("login executed");
+		
+	}
+	
+	@Test
+	public void customerinformation()
+	{
+		System.out.println("customer information verfied");
+	}
+	
+	
+	@Before //Annotation called before test case execution
+	public void setup()
+	{
+		System.out.println("Environment up and running" );
+		System.out.println("Connectivity is good");
+	}
+	
+	@After
+	public void teardown()
+	{
+		System.out.println("clear the cache");
+		System.out.println("Delete the auidt logs from the DB");
+	}
+	
+	
+}
+
+
